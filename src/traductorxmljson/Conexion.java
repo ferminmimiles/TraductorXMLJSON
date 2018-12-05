0ea6d646daf259a7f937bde915115a43f22cc93a
Fermin Mireles <Fermin Mireles@Crow>
2018-12-04 18:30:08 -0600

    Correccion
    Se corrigió la ruta de la clase Conexión

---
+++ src/traductorxmljson/Conexion.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tradicciones;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author alan_
+ */
+
+    import java.sql.*;
+public class Conexion {
+    private Connection conexion;
+    private Statement comando;
+   
+    @SuppressWarnings("empity-statement")
+    public boolean Conectar(String Host, String BD, String User, String Password) throws Exception{
+        try{
+            
+            this.conexion = DriverManager.getConnection("jdbc:mysql://" + Host + "/" + BD, User, Password);
+            this.comando = conexion.createStatement();
+            return true;
+        } catch(SQLException exc){
+            System.out.println("Error " + exc.toString());
+            return false;
+        }
+    }
+        
+        
+    public ResultSet ejecutarConsulta(String instruccionQL) throws SQLException{
+        ResultSet resultado = this.comando.executeQuery(instruccionQL);
+        return resultado;
+    }
+    
+    public int ejecutarActualizacion(String instruccionDML) throws SQLException {
+        int FilasAfectadas;
+        FilasAfectadas = this.comando.executeUpdate(instruccionDML);
+        System.out.println(instruccionDML + "Ejecutada");
+        return FilasAfectadas;
+    }
+
+}
+    
+
